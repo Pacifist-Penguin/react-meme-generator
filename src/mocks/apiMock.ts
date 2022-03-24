e769bcb2da8d7815644120326bc5f4628dfa8d2d
Pacifist-Penguin <75931217+Pacifist-Penguin@users.noreply.github.com>
2022-03-24 19:28:54 +0400

    remove garbage

--- src/mocks/apiMock.ts
+++
@@ -1,6 +0,0 @@
-export const imageArr = [
-    'https://upload.wikimedia.org/wikipedia/commons/f/fa/Hamster_in_hand.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/d/de/Pearl_Winter_White_Russian_Dwarf_Hamster_-_Front.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/e/e8/Sydney_the_hamster.jpg',
-  ];
-  
